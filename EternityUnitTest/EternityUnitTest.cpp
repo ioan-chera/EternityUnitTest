b54e623c1301c454d6a11afaba7e81b7601ac36f
Ioan Chera <ioan.chera@gmail.com>
2020-11-19 09:18:10 +0200

    Updated main project setting to link to the Google Test libraries.

--- EternityUnitTest/EternityUnitTest.cpp
+++ EternityUnitTest/EternityUnitTest.cpp
@@ -1,11 +1,11 @@
 // EternityUnitTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "gtest/gtest.h"
 
-int main()
+TEST(Hello, World)
 {
-    std::cout << "Hello World!\n";
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
